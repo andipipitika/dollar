e53c501a1fdccf5f7db6633be45d093b95e87d97
nrhdi <96911251+nrhdi@users.noreply.github.com>
2021-12-31 18:16:03 +0700

    Add files via upload

---
+++ nrhd.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b82ca1a17c870f9913aff93e5e096c82',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://accustomedinaccessible.com/b82ca1a17c870f9913aff93e5e096c82/invoke.js"></scr' + 'ipt>');
+</script>
