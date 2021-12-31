1624aac7c7e404050390704b6b11774766e95446
nrhdi <96911251+nrhdi@users.noreply.github.com>
2021-12-31 18:18:50 +0700

    Update nrhd.js

--- nrhd.js
+++ nrhd.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
 	atOptions = {
 		'key' : 'b82ca1a17c870f9913aff93e5e096c82',
 		'format' : 'iframe',
@@ -7,4 +7,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://accustomedinaccessible.com/b82ca1a17c870f9913aff93e5e096c82/invoke.js"></scr' + 'ipt>');
-</script>
