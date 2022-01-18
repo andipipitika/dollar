8a37f317e87d99745f6e2914f2fb01f6d8e67358
nrhdi <96911251+nrhdi@users.noreply.github.com>
2022-01-19 02:28:34 +0700

    Delete cantikmanis.js

--- cantikmanis.js
+++
@@ -1,9 +0,0 @@
-
-	atOptions = {
-		'key' : 'fdd8ca4af740f1d0f72a1d871caa9f11',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ef9i0f3oev47.com/fdd8ca4af740f1d0f72a1d871caa9f11/invoke.js"></scr' + 'ipt>');
