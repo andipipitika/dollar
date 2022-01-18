b28ccef255c83d7c2c168df733cd8e65469f0ce0
nrhdi <96911251+nrhdi@users.noreply.github.com>
2022-01-19 02:28:02 +0700

    Update cantikmanis.js

--- cantikmanis.js
+++ cantikmanis.js
@@ -7,6 +7,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ef9i0f3oev47.com/fdd8ca4af740f1d0f72a1d871caa9f11/invoke.js"></scr' + 'ipt>');
-
-
-//ef9i0f3oev47.com/68/f6/14/68f6147e5be670307b51dc4e5cdcb352.js
