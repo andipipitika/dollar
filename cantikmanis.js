149e62b9b0121c91079c5ba9f798a2be741bb24d
nrhdi <96911251+nrhdi@users.noreply.github.com>
2022-01-19 02:03:13 +0700

    Create cantikmanis.js

---
+++ cantikmanis.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'fdd8ca4af740f1d0f72a1d871caa9f11',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://ef9i0f3oev47.com/fdd8ca4af740f1d0f72a1d871caa9f11/invoke.js"></scr' + 'ipt>');
+</script>
+
+<script type='text/javascript' src='//ef9i0f3oev47.com/68/f6/14/68f6147e5be670307b51dc4e5cdcb352.js'></script>
